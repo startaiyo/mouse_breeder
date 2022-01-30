d7f26716440390e46675eacf96c989788d2bd3fb
startaiyo <startaiyo0104@gmail.com>
2022-01-30 19:52:47 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>hogehogehogehoge</h1><input></input><button>送信</button><table><th>aaa<th><th>bbb</th><tr><td>ccc</td></tr><td>ddd</td></table>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
