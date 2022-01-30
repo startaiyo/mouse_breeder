58f3b9cfe6e80c16dc57e998bcba289a5689eca1
startaiyo <startaiyo0104@gmail.com>
2022-01-31 08:09:30 +0900

    dockerized

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return '<h1>hogehogehogehoge</h1><input></input><button>送信</button><table><th>aaa<th><th>bbb</th><tr><td>ccc</td></tr><td>ddd</td></table>'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
