ac101d6f92979547f05beb6d9fea051445282337
startaiyo <startaiyo0104@gmail.com>
2022-02-06 09:37:03 +0900

    coding error 修正

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>hogehogehogehoge</h1><input></input><button>送信</button><table><th>aaa<th><th>bbb</th><tr><td>ccc</td></tr><td>ddd</td></table>'
+    return '<h1>hogehogehogehoge</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
