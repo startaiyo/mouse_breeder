11815134b1c166bdb91753c54f64f8018c42face
startaiyo <startaiyo0104@gmail.com>
2022-04-03 09:26:33 +0900

    不要コード削除

--- app/models/models.py
+++ app/models/models.py
@@ -12,18 +12,3 @@ class Mouse(db.Model):
     othergene = db.Column(db.String(255), default = None)
     dob = db.Column(db.DateTime, nullable = False)
     # # children = db.relationship("Mouse",cascade="delete")
-    # def regist_mouse(mouse):
-    #     record = Mouse(
-    #         gene1 = mouse['gene1'],
-    #         gene2 = mouse['gene2'],
-    #         othergene = mouse['othergene'],
-    #         dob = mouse['dob']
-    #     )
-    #     db.session.add(record)
-    #     db.session.commit()
-    #     return mouse
-
-# class MouseSchema(ma.ModelSchema):
-#     class Meta:
-#         model = Mouse
-#         fields = ('id','gene1','gene2','othergene','dob')
