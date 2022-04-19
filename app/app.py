b92131a99632a8b62de58c50ee4a280226113187
startaiyo <startaiyo0104@gmail.com>
2022-04-19 11:38:02 +0900

    画像表示に成功

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from database import init_db
 
 def create_app():
-    app = Flask(__name__,static_folder='static')
+    app = Flask(__name__,static_folder='src/static')
     app.config.from_object('config.Config')
     init_db(app)
     return app
