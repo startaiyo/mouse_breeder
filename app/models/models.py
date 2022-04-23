d9b60e25a39df412ca43cb73b16c312cb474a225
startaiyo <startaiyo0104@gmail.com>
2022-04-24 07:45:59 +0900

    tapleに

--- app/models/models.py
+++ app/models/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.schema import UniqueConstraint
 
 class Mouse(db.Model):
     __tablename__ = 'mice'
-    __table_args__=(UniqueConstraint("gene1", "gene2", "othergene", name="uq_user_00"))
+    __table_args__ = (db.UniqueConstraint("gene1", "gene2", "othergene", name="uq_user_00"),)
     id = db.Column(db.Integer, primary_key = True)
     gene1 = db.Column(db.String(255), nullable = False, default = "WT")
     gene2 = db.Column(db.String(255), nullable = False, default = "WT")
