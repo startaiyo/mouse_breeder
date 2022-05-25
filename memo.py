66ed12ff8882497deeadc0e2d80db990c738fdf4
startaiyo <startaiyo0104@gmail.com>
2022-05-25 23:35:58 +0900

    How to Go API

--- memo.py
+++ memo.py
@@ -1,3 +1,4 @@
 # docker-compose build flaskでrequirementの更新に対応できる。
 # mysqlのきどうmysql -u root -p -h 127.0.0.1 --port 3306
 # flask db migrateでdbの更新
+# Go APIの作り方：https://qiita.com/k-penguin-sato/items/8088b69304ee7e8f70be
